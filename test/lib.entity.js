5ffbf4b6918362bf141e75fd30fab6ccd4b86ff1
Nik Butenko <nikitabutenko@peoplebrowsr.com>
2014-03-10 23:02:33 +1100

    Updated tests to get better coverage

--- test/lib.entity.js
+++ test/lib.entity.js
@@ -12,6 +12,27 @@ describe('Gleam#entity', function () {
 		userData = require('./fixtures/user.json');
 	});
 
+	it('should throw error in case of incorrect namespace', function () {
+		function createNotExistingEntity() {
+			return gleam.entity('wrongSchema');
+		}
+		expect(createNotExistingEntity).to.throw(Error);
+	});
+
+	it('should throw error in case of wrong Entity schema', function () {
+		function createEntityWithWrongSchema() {
+			return gleam.entity('wrongSchema');
+		}
+		expect(createEntityWithWrongSchema).to.throw('Entity must be an object');
+	});
+
+	it('should throw error in case of Entity schema has properties and not methods', function () {
+		function createEntityWithFaultySchema() {
+			return gleam.entity('schemaWithProperties');
+		}
+		expect(createEntityWithFaultySchema).to.throw('Entity schema must not have any properties');
+	});
+
 	it('should create UserEntity', function () {
 		var entity = gleam.entity('user');
 		expect(entity).to.be.an('object');
@@ -37,6 +58,14 @@ describe('Gleam#entity', function () {
 		expect(entity.email()).to.equal(userData.email);
 	});
 
+	it('should throw Error if wrong defaults passed', function () {
+		function createEntityWithWrongData() {
+			return gleam.entity('user', 'not an object');
+		}
+		expect(createEntityWithWrongData).to.throw('Values must be an object');
+	});
+
+
 	it('should set defaults without setting modified flag', function () {
 		var entity = gleam.entity('user', userData);
 		expect(entity.modified().id).to.be.false;
@@ -49,12 +78,6 @@ describe('Gleam#entity', function () {
 		expect(entity.initial().id).to.equal(userData.id);
 		expect(entity.initial().name).to.equal(userData.name);
 		expect(entity.initial().email).to.equal(userData.email);
-	});
-
-	it('should throw error in case of incorrect namespace', function () {
-		expect(function () {
-			return gleam.entity('wrong/user');
-		}).to.throw(Error);
 	});
 
 });
